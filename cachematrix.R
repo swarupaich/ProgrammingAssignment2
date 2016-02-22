a11058af45c4ce723cf68cc430c2aeb871dbb632
swarupaich <swarup.aich@gmail.com>
2016-02-22 13:33:25 +0530

    commiting code of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The following makeCacheMatrix has 4 functions to set, get the "special" Matrix and 
+## setInverseMatrix and getInverseMatrix for the Inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inverseMatrix <- matrix()
+  
+  setMatrix <- function(y = matrix()) {
+    
+    x <<- y
+    
+  }
+  
+  getMatrix <- function() x
+  
+  setInverseMatrix <- function(x){
+    
+    inverseMatrix <<- solve(x)
+    
+  }
+  
+  getInverseMatrix <- function() inverseMatrix
+  
 }
 
 
-## Write a short comment describing this function
+## This function checks first if the Inverse Matrix is avaialble in cache, in that checking
+## also check if the Matrices are identical. If found in cache and matrices are identical, 
+## it returns the Inverse matrix. Else, it computes the Inverse and sets that in cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  
+  inverseMatrix <- x$getInverseMatrix()
+  
+  if(!is.null(inverseMatrix)){
+    
+    if(identical(x, x$getMatrix)){
+      
+      print("Cache data available")
+      return(inverseMatrix)
+      
+    }
+    
+  }
+  
+  inverseMatrix <- solve(x)
+  x$setInverseMatrix(x)
+  inverseMatrix
+  
 }
